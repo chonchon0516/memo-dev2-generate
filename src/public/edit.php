ef7a48b5283a5cfbd060a49f904c10c09681378e
chonchon0516 <azumi.toyoda@gmail.com>
2023-03-25 08:31:02 +0900

    memoアプリに機能追加してみよう②_①追加機能の確認をしましょう。

--- src/public/edit.php
+++ src/public/edit.php
@@ -6,6 +6,12 @@ $pdo = new PDO(
     $dbUserName,
     $dbPassword
 );
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+$sql = "SELECT * FROM pages where id = $id";
+$statement = $pdo->prepare($sql);
+$statement->execute();
+$page = $statement->fetch();
 ?>
 
 <body>
@@ -14,18 +20,19 @@ $pdo = new PDO(
 
   <form method="post" action="./update.php">
 
-    <input type="hidden" name="id">
+    <input type="hidden" name="id" value=<?php echo $page['id']; ?>>
 
     <div>
       <label for="name">タイトル
-        <input type="text" name="title">
+        <input type="text" name="title" value=<?php echo $page['title']; ?>>
       </label>
     </div>
 
     <div>
       <label for="content">感想
-        <input type="textarea" name="content">
-      </label>
+        <input type="textarea" name="content" value=<?php echo $page[
+            'content'
+        ]; ?>>
     </div>
 
     <button type="submit">更新</button>
